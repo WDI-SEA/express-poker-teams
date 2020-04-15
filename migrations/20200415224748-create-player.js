bf85bd6273c655301fea1f8266ebf401ee302a16
brandiw <brandimichelle.0911@gmail.com>
2020-04-15 15:52:34 -0700

    Set up models

---
+++ migrations/20200415224748-create-player.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('players', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      wins: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+      },
+      losses: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+      },
+      teamId: {
+        type: Sequelize.INTEGER
+      },
+      pic: {
+        type: Sequelize.STRING
+      },
+      bio: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('players');
+  }
+};
---
+++ models/player.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const player = sequelize.define('player', {
+    name: DataTypes.STRING,
+    wins: DataTypes.INTEGER,
+    losses: DataTypes.INTEGER,
+    teamId: DataTypes.INTEGER,
+    pic: DataTypes.STRING,
+    bio: DataTypes.TEXT
+  }, {});
+  player.associate = function(models) {
+    // A player belongs to a team
+    models.player.belongsTo(models.team)
+  };
+  return player;
+};
