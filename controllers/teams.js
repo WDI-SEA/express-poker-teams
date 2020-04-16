ff28f6f409a7da38c308c5aea53fd80d246214cb
brandiw <brandimichelle.0911@gmail.com>
2020-04-16 14:44:22 -0700

    Solution for part 1

--- controllers/teams.js
+++ controllers/teams.js
@@ -42,32 +42,4 @@ router.get('/:id', (req, res) => {
     })
 })
 
-router.get('/:id/win', (req, res) => {
-    db.player.update(
-        { wins: sequelize.literal('wins + 1') },
-        { where: { teamId: req.params.id } }
-    )
-    .then(() => {
-        res.redirect('/teams/' + req.params.id)
-    })
-    .catch(err => {
-        console.log(err)
-        res.render('error')
-    })
-})
-
-router.get('/:id/loss', (req, res) => {
-    db.player.update(
-        { losses: sequelize.literal('losses + 1') },
-        { where: { teamId: req.params.id } }
-    )
-    .then(() => {
-        res.redirect('/teams/' + req.params.id)
-    })
-    .catch(err => {
-        console.log(err)
-        res.render('error')
-    })
-})
-
 module.exports = router
